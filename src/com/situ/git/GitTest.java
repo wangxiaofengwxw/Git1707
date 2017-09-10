4b512e4f78d19e6c63e196e998b66f6f72f7f820
wangxiaofengwxw <592697159@qq.com>
2017-09-10 11:03:15 +0800

    第一次提交

---
+++ src/com/situ/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.situ.git;
+
+public class GitTest {
+	
+     public static void main(String[] args) {
+		System.out.println("helloGit");
+	}
+
+      public void A() {
+		System.out.println("GitTest.A()");
+	}
+
+      public void B() {
+  		System.out.println("GitTest.A()");
+  	}
+}
